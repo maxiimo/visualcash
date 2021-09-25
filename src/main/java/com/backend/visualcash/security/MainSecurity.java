7f1ca8d8c5467708c49953b06d9968484d2f1a54
maxiimo <fjcq93@gmail.com>
2021-09-25 09:25:46 -0500

    third push

--- src/main/java/com/backend/visualcash/security/MainSecurity.java
+++ src/main/java/com/backend/visualcash/security/MainSecurity.java
@@ -62,7 +62,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**", "/paquetes/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
