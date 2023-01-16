2509c02410cef9f30e02a389586cde0b17008797
maxiimo <fjcq93@gmail.com>
2023-01-16 18:44:02 -0500

    VisualcashApplication.java

--- src/main/java/com/backend/visualcash/VisualcashApplication.java
+++ src/main/java/com/backend/visualcash/VisualcashApplication.java
@@ -3,6 +3,7 @@ package com.backend.visualcash;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,7 @@ public class VisualcashApplication {
         SpringApplication.run(VisualcashApplication.class, args);
     }
     
+    @Profile({"dev","prod"})
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
